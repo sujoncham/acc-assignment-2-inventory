67e2b4ef9f5bce456810061aabaec67fe9b2e609
sujon-chambugong <sujonchambugong@gmail.com>
2022-09-20 12:05:47 +0600

    node_modules removing from github

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose')
-const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cors());
 
