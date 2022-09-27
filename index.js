1520b71726a3dfc959bd71412c3ba2a596a99e8a
sujon-chambugong <sujonchambugong@gmail.com>
2022-09-28 02:06:32 +0600

    testing index.js

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-
+const port = process.env.PORT || 5000;
 // tour get 
 app.get('/', (req, res)=>{
     res.send('Route is working! Yay!')
 });
+
+app.listen(port, ()=>{
+    console.log('port is', port);
+})
