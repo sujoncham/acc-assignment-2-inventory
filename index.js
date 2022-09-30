47528331ab026b34ea037a9c44c5a63d428b6c46
sujon-chambugong <sujonchambugong@gmail.com>
2022-10-01 01:06:15 +0600

    just checking

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+
 const app = require('./app')
+
 
 // tour get 
 app.get('/', (req, res)=>{
     res.send('Route is working! Yay!')
 });
 
-app.listen(port, ()=>{
-    console.log('port is', port);
-})
