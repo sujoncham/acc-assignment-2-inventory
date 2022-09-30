6c174e8e42c8c884cbb01412858d66ade33ffe4e
sujon-chambugong <sujonchambugong@gmail.com>
2022-10-01 01:35:52 +0600

    just checking

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+// tour get 
+app.get('/', (req, res)=>{
+    res.send('Route is working! Yay!')
+});
+
+app.listen(port, ()=>{
+    console.log('port is', port);
+})
