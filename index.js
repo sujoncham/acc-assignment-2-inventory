f4ec988b96f85f0ec1a7cdbe0d892b5c5194ec20
sujon-chambugong <sujonchambugong@gmail.com>
2022-10-01 01:00:57 +0600

    just checking

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose')
-
+const app = require('./app')
 app.use(express.json());
 app.use(cors());
-
-const tourRoute = require('./routes/tour.route');
 
 // tour get 
 app.get('/', (req, res)=>{
     res.send('Route is working! Yay!')
 });
 
-// tour posting 
-app.use('/api/v1/tour', tourRoute);
-
-module.exports = app;
+app.listen(port, ()=>{
+    console.log('port is', port);
+})
